b6ac0da337e918b55d4802ab6d3494417967d431
25lp5214 User <25lp5214@iMac-de-Admin.local>
2025-12-22 20:24:57 -0800

    a

---
+++ Project-12/script.js
@@ -0,0 +1,73 @@
+const input = document.querySelector(".input");
+const submit = document.getElementById("sumbitBtn");
+const result = document.querySelector(".result");
+const clear = document.getElementById("clearBtn");
+
+let items = [];
+let itemId = 1;
+
+const submitValue = () => {
+  const value = input.value;
+
+  const item = {
+    id: itemId,
+    text: value,
+  };
+
+  if (value === "") {
+    window.alert("Enter a Text");
+    return null;
+  } else {
+    items.push(item);
+  }
+
+  itemId++;
+
+  renderTasks();
+  clearInput();
+};
+
+const renderTasks = () => {
+  let itemElementHTML = "";
+
+  let forItems = items;
+
+  forItems.forEach((item) => {
+    itemElementHTML += createItems(item);
+  });
+
+  console.log(itemElementHTML);
+
+  result.innerHTML = itemElementHTML;
+};
+
+const createItems = (item) => {
+  return `<div class="space">
+            <div class="result-value">${item.text}</div>
+            <div class="icons">
+              <button class="edit-button">
+                <i class="fa-solid fa-pen-to-square"></i>
+              </button>
+              <button class="trash-button" onclick="deleteItem(${item.id})">
+                <i class="fa-solid fa-trash"></i>
+              </button>
+            </div>
+          </div>`;
+};
+
+const deleteItem = (itemId) => {
+  items = items.filter((item) => item.id !== itemId);
+  renderTasks();
+};
+
+const clearInput = () => {
+  input.value = "";
+};
+
+const clearValue = () => {
+  items = [];
+  renderTasks();
+};
+
+submit.addEventListener("click", submitValue);
+clear.addEventListener("click", clearValue);
