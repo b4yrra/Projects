61e8415bae6a9fc3074af89f831f20a3884eee0a
b4yrra <t.chinbayr@gmail.com>
2025-12-23 13:15:56 +0800

    a

--- Project-1/script.js
+++ Project-1/script.js
@@ -7,15 +7,34 @@ let num = numCount.textContent;
 
 incBtn.addEventListener("click", () => {
   num++;
+
+  if (num > 0) {
+    countNum.style.color = "green";
+  } else if (num === 0) {
+    countNum.style.color = "black";
+  }
+
   numCount.textContent = num;
 });
 
 decBtn.addEventListener("click", () => {
   num--;
+
+  if (num < 0) {
+    countNum.style.color = "red";
+  } else if (num === 0) {
+    countNum.style.color = "black";
+  }
+
   numCount.textContent = num;
 });
 
 resBtn.addEventListener("click", () => {
   num = 0;
+
+  if (num === 0) {
+    countNum.style.color = "black";
+  }
+
   numCount.textContent = num;
 });
